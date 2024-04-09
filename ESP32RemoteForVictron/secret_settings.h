5e98c1626a28aafc6213cd381828b8ab460dd6b2
Rob Latour <5200730+roblatour@users.noreply.github.com>
2024-04-08 20:04:38 -0400

    Update secret_settings.h

--- ESP32RemoteForVictron/secret_settings.h
+++ ESP32RemoteForVictron/secret_settings.h
@@ -1,6 +1,6 @@
 
 #define SECRET_SETTINGS_WIFI_SSID               "xxxxx"                     // Your Wi-Fi Station ID which is on the same network as your Victron Venus device
-#define SECRET_SETTINGS_WIFI_PASSWORD           "yyyyy"                     // Your Wi-Fi 
+#define SECRET_SETTINGS_WIFI_PASSWORD           "yyyyy"                     // Your Wi-Fi Password
 
 #define SECRET_SETTING_VICTRON_INSTALLATION_ID  "+"                         // your unique Victron Installation/Portal ID. If you don't know it you can use a "+" in this field - but it will be more efficient to use the actual ID.
 #define SECRET_SETTING_VICTRON_MULTIPLUS_ID     "+"                         // your unique Victron Multiplus (vebus) three digit ID; if you don't know it you can use a "+" in this field but it will be more efficient to use the actual ID.
