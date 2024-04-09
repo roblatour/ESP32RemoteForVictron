90101422503329a08f981dc47561be42cf20d85f
Rob Latour <5200730+roblatour@users.noreply.github.com>
2024-04-08 20:06:00 -0400

    Update secret_settings.h

--- ESP32RemoteForVictron/secret_settings.h
+++ ESP32RemoteForVictron/secret_settings.h
@@ -1,15 +1,15 @@
 
-#define SECRET_SETTINGS_WIFI_SSID               "xxxxx"                     // Your Wi-Fi Station ID which is on the same network as your Victron Venus device
-#define SECRET_SETTINGS_WIFI_PASSWORD           "yyyyy"                     // Your Wi-Fi Password
+#define SECRET_SETTINGS_WIFI_SSID               "xxxxx"                     // your Wi-Fi Station ID which is on the same network as your Victron Venus device
+#define SECRET_SETTINGS_WIFI_PASSWORD           "yyyyy"                     // your Wi-Fi Password
 
-#define SECRET_SETTING_VICTRON_INSTALLATION_ID  "+"                         // your unique Victron Installation/Portal ID. If you don't know it you can use a "+" in this field - but it will be more efficient to use the actual ID.
-#define SECRET_SETTING_VICTRON_MULTIPLUS_ID     "+"                         // your unique Victron Multiplus (vebus) three digit ID; if you don't know it you can use a "+" in this field but it will be more efficient to use the actual ID.
+#define SECRET_SETTING_VICTRON_INSTALLATION_ID  "+"                         // your unique Victron Installation/Portal ID. If you don't know it you can use a "+" in this field but it will be more efficient to use the actual ID
+#define SECRET_SETTING_VICTRON_MULTIPLUS_ID     "+"                         // your unique Victron Multiplus (vebus) three digit ID; if you don't know it you can use a "+" in this field but it will be more efficient to use the actual ID
                                                                             // note: in either or both case above if you use a '+' you can look in the Serial Monitor output to see what the discovered IDs are, and then come back and change the setting(s) above
 
 
 #define MQTTBroker                              "venus.local"              // may also be an IP address such as 192.168.1.195
 #define MQTTUserID                              "Anonymous"                // if you have MQTT security setup, you can enter your MQQT User ID here, otherwise leave the set to  "Anonymous"
-#define MQTTPassword                            "Anonymous"                // if you have MQTT security setup, you can enter your MQQT password here, otherwise leave the set to  "Anonymous"
+#define MQTTPassword                            "Anonymous"                // if you have MQTT security setup, you can enter your MQQT Password here, otherwise leave the set to  "Anonymous"
 #define MQTTClientName                          "ESP32RemoteForVictron"
 #define MQTTPort                                1883
 
